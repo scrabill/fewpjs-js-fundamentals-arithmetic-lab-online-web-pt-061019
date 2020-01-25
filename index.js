65b06b28df9309d230890dca8d67846c3a285de9
scrabill <shannon.crabill@gmail.com>
2020-01-24 22:39:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
 // Write your code here
+
+// array = [1,2,20]
+//
+// function max(array) {
+//   Math.max(array);
+// }
+
+const num1 = 2
+const num2 = 31
+
+const multiply = num1 * num2
+const random = Math.floor()
+const mod = 20 & 4
+
+const max = Math.max(1,2,20)
+
+
+// Create a variable called multiply that will multiply the variables num1 and num2 whose results will equal "62".
+// Create a variable called random that will generate a random integer greater than 0.
+// Create a variable called mod set to an equation whose remainder is equal to "4".
+// Create a variable called max that will return "20" as the highest number in the set.
